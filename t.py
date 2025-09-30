c860b8c22157a617ffb1d7245d812b340cf99bfb
rimineuit <minh0974680144@gmail.com>
2025-09-30 09:46:53 +0700

    tăng thời gian chờ crawl tiktok

--- t.py
+++ t.py
@@ -6,7 +6,7 @@ import sys
 
 with sync_playwright() as p:
     browser = p.chromium.launch(
-        headless=False,
+        headless=True,
         args=[
             "--disable-blink-features=AutomationControlled",
             "--no-sandbox",
@@ -23,21 +23,12 @@ with sync_playwright() as p:
         java_script_enabled=True
     )
     page = context.new_page()
-    url = "https://www.google.com/maps/place/PNJ+632+Quang+Trung/@10.8360141,106.6416451,15z/data=!4m10!1m2!2m1!1zQ-G7rWEgSMOgbmcgVHJhbmcgU-G7qWMgUG5q!3m6!1s0x317529a76c3a7b25:0xa2d7488a32038f49!8m2!3d10.8360141!4d106.6606995!15sChtD4butYSBIw6BuZyBUcmFuZyBT4bupYyBQbmoiA4gBAVodIhtj4butYSBow6BuZyB0cmFuZyBz4bupYyBwbmqSAQ1qZXdlbHJ5X3N0b3JlmgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVF6T1hGaWFXeFJSUkFCqgF0Cg0vZy8xMWZreXlmMnJmEAEqHyIbY-G7rWEgaMOgbmcgdHJhbmcgc-G7qWMgcG5qKEIyHxABIhtVh7qHTy2Va09YOYg7ov-AaRC0ROwxjPT3HFsyHxACIhtj4butYSBow6BuZyB0cmFuZyBz4bupYyBwbmrgAQD6AQQIABAw!16s%2Fg%2F11csr560l1?entry=ttu&g_ep=EgoyMDI1MDkyMy4wIKXMDSoASAFQAw%3D%3D"
+    url = "https://www.tiktok.com/@cotuyenhoala/video/7527196260919512328"
     
 
     page.goto(url)
+    page.wait_for_load_state("networkidle")
+    page.evaluate('window.scrollBy(0, window.innerHeight);')
     time.sleep(10)
-    page.wait_for_selector("""
-        document.querySelector("#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde")
-                .scrollBy(0, 300)
-        """)
-    for i in range(10):
-        # Scroll xuống 300px
-        print("Scroll")
-        page.evaluate("""
-        document.querySelector("#QA0Szd > div > div > div.w6VYqd > div.bJzME.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde")
-                .scrollBy(0, 300)
-        """)
-        time.sleep(2)
+    page.evaluate('window.scrollBy(0, window.innerHeight);')
     time.sleep(10)
